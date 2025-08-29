f213089ab13b8e363f4aeda971afadb42c4d02d6
alamshuvo <alamshuvozeny019@gmail.com>
2025-08-29 23:03:30 +0600

    about page added

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <p>this is about pages</p>
+        </div>
+    );
+};
+
+export default About;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
+import About from "../pages/About";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home></Home>,
   },
+  {
+    path:"/about",
+    element:<About></About>
+  }
 ]);
 
 export default router
